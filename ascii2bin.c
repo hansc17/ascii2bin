3edf5a6f555f8366b56e7e6167454ec1201163a2
hansc17 <78043403+hansc17@users.noreply.github.com>
2021-03-04 12:58:11 -0800

    ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -3,18 +3,33 @@
 
 int main()
 {
+    int retval;
     int offset = 48;  
     int number = 0;
-    char ascii_value;
+    int error = 0;
+    char ascii_value = 0;
+    char digit = 0;
     
-    int retval = read(0, &ascii_value, 1);
+    
+    retval = read(0, &ascii_value, 1);
     while ((retval == 1) && (ascii_value != '\n')) {
+        
+        if(ascii_value == offset || ascii_value == 49) {
         int digit = ascii_value - offset;
-        if ((digit == 1) || (digit == 0))
-            number = (number << 1) + digit;  
+        number = (number << 1) + digit;  
         retval = read(0, &ascii_value, 1);
-    }    
+    } 
+    else {
+         error = error + 1;
+         printf("%d\n", error);
+         break;
+         }
+    }
+    if (error == 0) {  
     printf("%d\n", number);
-
     return 0;
-}
+    }
+    else{
+    return 1;
+    }
+   }
